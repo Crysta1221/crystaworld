b148933ee4e1482c587af3072597c6f2dd2ee33a
Crysta1221 <takuyakojim@outlook.jp>
2023-09-06 15:44:06 +0900

    Add Contact and Donate

--- src/components/view/contact.vue
+++ src/components/view/contact.vue
@@ -1,12 +1,281 @@
 <template>
-    <navbar/>
+  <navbar />
+  <link rel="stylesheet" href="https://cdn.tailgrids.com/tailgrids-fallback.css" />
+  <!-- ====== Contact Section Start -->
+  <section class="bg-white py-20 lg:py-[120px] overflow-hidden relative z-10">
+    <div class="container">
+      <div class="flex flex-wrap lg:justify-between -mx-4">
+        <div class="w-full lg:w-1/2 xl:w-6/12 px-4">
+          <div class="max-w-[570px] mb-12 lg:mb-0">
+            <span class="block mb-4 text-base text-primary font-semibold">
+              Contact Us
+            </span>
+            <h2 class="
+                     text-dark
+                     mb-6
+                     uppercase
+                     font-bold
+                     text-[32px]
+                     sm:text-[40px]
+                     lg:text-[36px]
+                     xl:text-[40px]
+                     ">
+              お問い合わせ
+            </h2>
+            <p class="text-base text-body-color leading-relaxed mb-9">
+              Webサービス、サイトに関するお問い合わせや依頼等ありましたらこちらのフォームにご記入の上、送信してください。
+            </p>
+
+          </div>
+        </div>
+        <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+          <div class="bg-white relative rounded-lg p-8 sm:p-12 shadow-lg">
+            <form @submit.prevent="request">
+              <div class="mb-6">
+                <input type="text" id="name" name="name" placeholder="お名前" class="
+                           w-full
+                           rounded
+                           py-3
+                           px-[14px]
+                           text-body-color text-base
+                           border border-[f0f0f0]
+                           outline-none
+                           focus-visible:shadow-none
+                           focus:border-primary
+                           " required />
+              </div>
+              <div class="mb-6">
+                <input type="email" id="email" name="email" placeholder="メールアドレス" class="
+                           w-full
+                           rounded
+                           py-3
+                           px-[14px]
+                           text-body-color text-base
+                           border border-[f0f0f0]
+                           outline-none
+                           focus-visible:shadow-none
+                           focus:border-primary
+                           " required />
+              </div>
+              <div class="mb-6">
+                <input type="text" id="title" name="title" placeholder="件名" class="
+                           w-full
+                           rounded
+                           py-3
+                           px-[14px]
+                           text-body-color text-base
+                           border border-[f0f0f0]
+                           outline-none
+                           focus-visible:shadow-none
+                           focus:border-primary
+                           " required />
+              </div>
+              <div class="mb-6">
+                <textarea rows="6" id="detail" name="detail" placeholder="内容" class="
+                           w-full
+                           rounded
+                           py-3
+                           px-[14px]
+                           text-body-color text-base
+                           border border-[f0f0f0]
+                           resize-none
+                           outline-none
+                           focus-visible:shadow-none
+                           focus:border-primary
+                           " required></textarea>
+              </div>
+              <div>
+                <button :disabled="sent" type="submit" class="
+                           w-full
+                           bg-blue-500
+                           text-white
+                           bg-primary
+                           rounded
+                           border border-primary
+                           p-3
+                           transition
+                           hover:bg-blue-800
+                           active:bg-blue-800
+                           ">
+                  送信
+                </button>
+              </div>
+            </form>
+            <div role="alert"
+              class="mt-8 flex items-start md:items-center shadow-md bg-positive-100 pr-2 pl-4 ring-1 ring-positive-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+              v-if="sent">
+              <SfIconCheckCircle class="my-2 mr-2 text-positive-700 shrink-0" />
+              <p class="py-2 mr-2">フォームは正しく送信されました! 数秒後にリダイレクトします...</p>
+            </div>
+            <div>
+              <span class="absolute -top-10 -right-9 z-[-1]">
+                <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M0 100C0 44.7715 0 0 0 0C55.2285 0 100 44.7715 100 100C100 100 100 100 0 100Z" fill="#3056D3" />
+                </svg>
+              </span>
+              <span class="absolute -right-10 top-[90px] z-[-1]">
+                <svg width="34" height="134" viewBox="0 0 34 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="31.9993" cy="132" r="1.66667" transform="rotate(180 31.9993 132)" fill="#13C296" />
+                  <circle cx="31.9993" cy="117.333" r="1.66667" transform="rotate(180 31.9993 117.333)" fill="#13C296" />
+                  <circle cx="31.9993" cy="102.667" r="1.66667" transform="rotate(180 31.9993 102.667)" fill="#13C296" />
+                  <circle cx="31.9993" cy="88" r="1.66667" transform="rotate(180 31.9993 88)" fill="#13C296" />
+                  <circle cx="31.9993" cy="73.3333" r="1.66667" transform="rotate(180 31.9993 73.3333)" fill="#13C296" />
+                  <circle cx="31.9993" cy="45" r="1.66667" transform="rotate(180 31.9993 45)" fill="#13C296" />
+                  <circle cx="31.9993" cy="16" r="1.66667" transform="rotate(180 31.9993 16)" fill="#13C296" />
+                  <circle cx="31.9993" cy="59" r="1.66667" transform="rotate(180 31.9993 59)" fill="#13C296" />
+                  <circle cx="31.9993" cy="30.6666" r="1.66667" transform="rotate(180 31.9993 30.6666)" fill="#13C296" />
+                  <circle cx="31.9993" cy="1.66665" r="1.66667" transform="rotate(180 31.9993 1.66665)" fill="#13C296" />
+                  <circle cx="17.3333" cy="132" r="1.66667" transform="rotate(180 17.3333 132)" fill="#13C296" />
+                  <circle cx="17.3333" cy="117.333" r="1.66667" transform="rotate(180 17.3333 117.333)" fill="#13C296" />
+                  <circle cx="17.3333" cy="102.667" r="1.66667" transform="rotate(180 17.3333 102.667)" fill="#13C296" />
+                  <circle cx="17.3333" cy="88" r="1.66667" transform="rotate(180 17.3333 88)" fill="#13C296" />
+                  <circle cx="17.3333" cy="73.3333" r="1.66667" transform="rotate(180 17.3333 73.3333)" fill="#13C296" />
+                  <circle cx="17.3333" cy="45" r="1.66667" transform="rotate(180 17.3333 45)" fill="#13C296" />
+                  <circle cx="17.3333" cy="16" r="1.66667" transform="rotate(180 17.3333 16)" fill="#13C296" />
+                  <circle cx="17.3333" cy="59" r="1.66667" transform="rotate(180 17.3333 59)" fill="#13C296" />
+                  <circle cx="17.3333" cy="30.6666" r="1.66667" transform="rotate(180 17.3333 30.6666)" fill="#13C296" />
+                  <circle cx="17.3333" cy="1.66665" r="1.66667" transform="rotate(180 17.3333 1.66665)" fill="#13C296" />
+                  <circle cx="2.66536" cy="132" r="1.66667" transform="rotate(180 2.66536 132)" fill="#13C296" />
+                  <circle cx="2.66536" cy="117.333" r="1.66667" transform="rotate(180 2.66536 117.333)" fill="#13C296" />
+                  <circle cx="2.66536" cy="102.667" r="1.66667" transform="rotate(180 2.66536 102.667)" fill="#13C296" />
+                  <circle cx="2.66536" cy="88" r="1.66667" transform="rotate(180 2.66536 88)" fill="#13C296" />
+                  <circle cx="2.66536" cy="73.3333" r="1.66667" transform="rotate(180 2.66536 73.3333)" fill="#13C296" />
+                  <circle cx="2.66536" cy="45" r="1.66667" transform="rotate(180 2.66536 45)" fill="#13C296" />
+                  <circle cx="2.66536" cy="16" r="1.66667" transform="rotate(180 2.66536 16)" fill="#13C296" />
+                  <circle cx="2.66536" cy="59" r="1.66667" transform="rotate(180 2.66536 59)" fill="#13C296" />
+                  <circle cx="2.66536" cy="30.6666" r="1.66667" transform="rotate(180 2.66536 30.6666)" fill="#13C296" />
+                  <circle cx="2.66536" cy="1.66665" r="1.66667" transform="rotate(180 2.66536 1.66665)" fill="#13C296" />
+                </svg>
+              </span>
+              <span class="absolute -left-7 -bottom-7 z-[-1]">
+                <svg width="107" height="134" viewBox="0 0 107 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="104.999" cy="132" r="1.66667" transform="rotate(180 104.999 132)" fill="#13C296" />
+                  <circle cx="104.999" cy="117.333" r="1.66667" transform="rotate(180 104.999 117.333)" fill="#13C296" />
+                  <circle cx="104.999" cy="102.667" r="1.66667" transform="rotate(180 104.999 102.667)" fill="#13C296" />
+                  <circle cx="104.999" cy="88" r="1.66667" transform="rotate(180 104.999 88)" fill="#13C296" />
+                  <circle cx="104.999" cy="73.3333" r="1.66667" transform="rotate(180 104.999 73.3333)" fill="#13C296" />
+                  <circle cx="104.999" cy="45" r="1.66667" transform="rotate(180 104.999 45)" fill="#13C296" />
+                  <circle cx="104.999" cy="16" r="1.66667" transform="rotate(180 104.999 16)" fill="#13C296" />
+                  <circle cx="104.999" cy="59" r="1.66667" transform="rotate(180 104.999 59)" fill="#13C296" />
+                  <circle cx="104.999" cy="30.6666" r="1.66667" transform="rotate(180 104.999 30.6666)" fill="#13C296" />
+                  <circle cx="104.999" cy="1.66665" r="1.66667" transform="rotate(180 104.999 1.66665)" fill="#13C296" />
+                  <circle cx="90.3333" cy="132" r="1.66667" transform="rotate(180 90.3333 132)" fill="#13C296" />
+                  <circle cx="90.3333" cy="117.333" r="1.66667" transform="rotate(180 90.3333 117.333)" fill="#13C296" />
+                  <circle cx="90.3333" cy="102.667" r="1.66667" transform="rotate(180 90.3333 102.667)" fill="#13C296" />
+                  <circle cx="90.3333" cy="88" r="1.66667" transform="rotate(180 90.3333 88)" fill="#13C296" />
+                  <circle cx="90.3333" cy="73.3333" r="1.66667" transform="rotate(180 90.3333 73.3333)" fill="#13C296" />
+                  <circle cx="90.3333" cy="45" r="1.66667" transform="rotate(180 90.3333 45)" fill="#13C296" />
+                  <circle cx="90.3333" cy="16" r="1.66667" transform="rotate(180 90.3333 16)" fill="#13C296" />
+                  <circle cx="90.3333" cy="59" r="1.66667" transform="rotate(180 90.3333 59)" fill="#13C296" />
+                  <circle cx="90.3333" cy="30.6666" r="1.66667" transform="rotate(180 90.3333 30.6666)" fill="#13C296" />
+                  <circle cx="90.3333" cy="1.66665" r="1.66667" transform="rotate(180 90.3333 1.66665)" fill="#13C296" />
+                  <circle cx="75.6654" cy="132" r="1.66667" transform="rotate(180 75.6654 132)" fill="#13C296" />
+                  <circle cx="31.9993" cy="132" r="1.66667" transform="rotate(180 31.9993 132)" fill="#13C296" />
+                  <circle cx="75.6654" cy="117.333" r="1.66667" transform="rotate(180 75.6654 117.333)" fill="#13C296" />
+                  <circle cx="31.9993" cy="117.333" r="1.66667" transform="rotate(180 31.9993 117.333)" fill="#13C296" />
+                  <circle cx="75.6654" cy="102.667" r="1.66667" transform="rotate(180 75.6654 102.667)" fill="#13C296" />
+                  <circle cx="31.9993" cy="102.667" r="1.66667" transform="rotate(180 31.9993 102.667)" fill="#13C296" />
+                  <circle cx="75.6654" cy="88" r="1.66667" transform="rotate(180 75.6654 88)" fill="#13C296" />
+                  <circle cx="31.9993" cy="88" r="1.66667" transform="rotate(180 31.9993 88)" fill="#13C296" />
+                  <circle cx="75.6654" cy="73.3333" r="1.66667" transform="rotate(180 75.6654 73.3333)" fill="#13C296" />
+                  <circle cx="31.9993" cy="73.3333" r="1.66667" transform="rotate(180 31.9993 73.3333)" fill="#13C296" />
+                  <circle cx="75.6654" cy="45" r="1.66667" transform="rotate(180 75.6654 45)" fill="#13C296" />
+                  <circle cx="31.9993" cy="45" r="1.66667" transform="rotate(180 31.9993 45)" fill="#13C296" />
+                  <circle cx="75.6654" cy="16" r="1.66667" transform="rotate(180 75.6654 16)" fill="#13C296" />
+                  <circle cx="31.9993" cy="16" r="1.66667" transform="rotate(180 31.9993 16)" fill="#13C296" />
+                  <circle cx="75.6654" cy="59" r="1.66667" transform="rotate(180 75.6654 59)" fill="#13C296" />
+                  <circle cx="31.9993" cy="59" r="1.66667" transform="rotate(180 31.9993 59)" fill="#13C296" />
+                  <circle cx="75.6654" cy="30.6666" r="1.66667" transform="rotate(180 75.6654 30.6666)" fill="#13C296" />
+                  <circle cx="31.9993" cy="30.6666" r="1.66667" transform="rotate(180 31.9993 30.6666)" fill="#13C296" />
+                  <circle cx="75.6654" cy="1.66665" r="1.66667" transform="rotate(180 75.6654 1.66665)" fill="#13C296" />
+                  <circle cx="31.9993" cy="1.66665" r="1.66667" transform="rotate(180 31.9993 1.66665)" fill="#13C296" />
+                  <circle cx="60.9993" cy="132" r="1.66667" transform="rotate(180 60.9993 132)" fill="#13C296" />
+                  <circle cx="17.3333" cy="132" r="1.66667" transform="rotate(180 17.3333 132)" fill="#13C296" />
+                  <circle cx="60.9993" cy="117.333" r="1.66667" transform="rotate(180 60.9993 117.333)" fill="#13C296" />
+                  <circle cx="17.3333" cy="117.333" r="1.66667" transform="rotate(180 17.3333 117.333)" fill="#13C296" />
+                  <circle cx="60.9993" cy="102.667" r="1.66667" transform="rotate(180 60.9993 102.667)" fill="#13C296" />
+                  <circle cx="17.3333" cy="102.667" r="1.66667" transform="rotate(180 17.3333 102.667)" fill="#13C296" />
+                  <circle cx="60.9993" cy="88" r="1.66667" transform="rotate(180 60.9993 88)" fill="#13C296" />
+                  <circle cx="17.3333" cy="88" r="1.66667" transform="rotate(180 17.3333 88)" fill="#13C296" />
+                  <circle cx="60.9993" cy="73.3333" r="1.66667" transform="rotate(180 60.9993 73.3333)" fill="#13C296" />
+                  <circle cx="17.3333" cy="73.3333" r="1.66667" transform="rotate(180 17.3333 73.3333)" fill="#13C296" />
+                  <circle cx="60.9993" cy="45" r="1.66667" transform="rotate(180 60.9993 45)" fill="#13C296" />
+                  <circle cx="17.3333" cy="45" r="1.66667" transform="rotate(180 17.3333 45)" fill="#13C296" />
+                  <circle cx="60.9993" cy="16" r="1.66667" transform="rotate(180 60.9993 16)" fill="#13C296" />
+                  <circle cx="17.3333" cy="16" r="1.66667" transform="rotate(180 17.3333 16)" fill="#13C296" />
+                  <circle cx="60.9993" cy="59" r="1.66667" transform="rotate(180 60.9993 59)" fill="#13C296" />
+                  <circle cx="17.3333" cy="59" r="1.66667" transform="rotate(180 17.3333 59)" fill="#13C296" />
+                  <circle cx="60.9993" cy="30.6666" r="1.66667" transform="rotate(180 60.9993 30.6666)" fill="#13C296" />
+                  <circle cx="17.3333" cy="30.6666" r="1.66667" transform="rotate(180 17.3333 30.6666)" fill="#13C296" />
+                  <circle cx="60.9993" cy="1.66665" r="1.66667" transform="rotate(180 60.9993 1.66665)" fill="#13C296" />
+                  <circle cx="17.3333" cy="1.66665" r="1.66667" transform="rotate(180 17.3333 1.66665)" fill="#13C296" />
+                  <circle cx="46.3333" cy="132" r="1.66667" transform="rotate(180 46.3333 132)" fill="#13C296" />
+                  <circle cx="2.66536" cy="132" r="1.66667" transform="rotate(180 2.66536 132)" fill="#13C296" />
+                  <circle cx="46.3333" cy="117.333" r="1.66667" transform="rotate(180 46.3333 117.333)" fill="#13C296" />
+                  <circle cx="2.66536" cy="117.333" r="1.66667" transform="rotate(180 2.66536 117.333)" fill="#13C296" />
+                  <circle cx="46.3333" cy="102.667" r="1.66667" transform="rotate(180 46.3333 102.667)" fill="#13C296" />
+                  <circle cx="2.66536" cy="102.667" r="1.66667" transform="rotate(180 2.66536 102.667)" fill="#13C296" />
+                  <circle cx="46.3333" cy="88" r="1.66667" transform="rotate(180 46.3333 88)" fill="#13C296" />
+                  <circle cx="2.66536" cy="88" r="1.66667" transform="rotate(180 2.66536 88)" fill="#13C296" />
+                  <circle cx="46.3333" cy="73.3333" r="1.66667" transform="rotate(180 46.3333 73.3333)" fill="#13C296" />
+                  <circle cx="2.66536" cy="73.3333" r="1.66667" transform="rotate(180 2.66536 73.3333)" fill="#13C296" />
+                  <circle cx="46.3333" cy="45" r="1.66667" transform="rotate(180 46.3333 45)" fill="#13C296" />
+                  <circle cx="2.66536" cy="45" r="1.66667" transform="rotate(180 2.66536 45)" fill="#13C296" />
+                  <circle cx="46.3333" cy="16" r="1.66667" transform="rotate(180 46.3333 16)" fill="#13C296" />
+                  <circle cx="2.66536" cy="16" r="1.66667" transform="rotate(180 2.66536 16)" fill="#13C296" />
+                  <circle cx="46.3333" cy="59" r="1.66667" transform="rotate(180 46.3333 59)" fill="#13C296" />
+                  <circle cx="2.66536" cy="59" r="1.66667" transform="rotate(180 2.66536 59)" fill="#13C296" />
+                  <circle cx="46.3333" cy="30.6666" r="1.66667" transform="rotate(180 46.3333 30.6666)" fill="#13C296" />
+                  <circle cx="2.66536" cy="30.6666" r="1.66667" transform="rotate(180 2.66536 30.6666)" fill="#13C296" />
+                  <circle cx="46.3333" cy="1.66665" r="1.66667" transform="rotate(180 46.3333 1.66665)" fill="#13C296" />
+                  <circle cx="2.66536" cy="1.66665" r="1.66667" transform="rotate(180 2.66536 1.66665)" fill="#13C296" />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ====== Contact Section End -->
+  <basefooter />
 </template>
 
 <script>
 import navbar from "@/components/layout/navbar.vue";
+import basefooter from "@/components/layout/basefooter.vue";
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      sent: false,
+    }
+  },
   components: {
-    navbar
+    navbar,
+    basefooter,
+  },
+  methods: {
+    request: async function () {
+      var name = "entry.610397140" + "=" + document.getElementById("name").value;
+      var mail = "entry.672450685" + "=" + document.getElementById("email").value;
+      var method = "entry.472324914" + "=" + document.getElementById("title").value;
+      var code = "entry.1673815383" + "=" + document.getElementById("detail").value;
+
+      var url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeK2rwPA3FOHbI4vxW54GIDAvU1rhPeZ6RzQMhsCsedjp6mdQ/formResponse?" +
+        name + "&" + mail + "&" + method + "&" + code;
+
+      if (name && mail && method && code) {
+        axios.get(url);
+        this.sent = true;
+        setTimeout(() => {
+          window.location.href = "/contact";
+          this.sent = false;
+        }, 5000);
+      }
+    }
   }
 }
 </script>
--- src/components/view/donate.vue
+++ src/components/view/donate.vue
@@ -6,47 +6,49 @@ import { ref } from 'vue';
 </script>
 
 <template>
-    <navbar />
-    <div class="relative min-h-[600px] mt-12">
-      <picture>
-        <source srcset="/background.svg" media="(min-width: 768px)" style="@media (min-width:767px) {height: 80%;}"/>
-        <img
-          src="/background.svg"
-          class="absolute w-full md:h-[80%] sm:h-[40%] z-[-1] md:object-cover"
-        />
-      </picture>
-      <div class="md:flex md:flex-row-reverse md:justify-center max-w[1536px] mx-auto md:min-h-[600px]">
-        <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-        </div>
-        <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
+  <navbar />
+  <div class="relative min-h-[600px] mt-12">
+    <picture>
+      <source srcset="/background.svg" media="(min-width: 768px)" style="@media (min-width:767px) {height: 80%;}" />
+      <img src="/background.svg" class="absolute w-full md:h-[80%] sm:h-[40%] z-[-1] md:object-cover" />
+    </picture>
+    <div class="md:flex md:flex-row-reverse md:justify-center max-w[1536px] mx-auto md:min-h-[600px]">
+      <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
+      </div>
+      <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
         <h1 class="typography-display-2 md:typography-display-1 md:leading-[67.5px] font-bold mt-2 mb-4 p-8">
           寄付
         </h1>
       </div>
-      </div>
     </div>
-    <div class="prose max-w-none text-center ml-10 -mt-96 md:-mt-20 lg:-mt-24 mr-10">
-      <Donate/>
-    </div>
-    <div class="flex justify-center mt-2">
-      <a href="https://paypal.me/softcrystal1" target="_blank" class="flex items-center text-blue-700 hover:text-blue-900 active:text-blue-900">
-        <span class="mr-2">Paypalでの寄付はこちら</span>
-        <Icon icon="mdi:open-in-new" width="22" height="22"/>
-      </a>
-    </div>
-    <div class="container mx-auto mt-8 mb-24">
-      <form @submit.prevent="request">
+  </div>
+  <div class="prose max-w-none text-center ml-10 -mt-96 md:-mt-20 lg:-mt-24 mr-10">
+    <Donate />
+  </div>
+  <div class="flex justify-center mt-2">
+    <a href="https://paypal.me/softcrystal1" target="_blank"
+      class="flex items-center text-blue-700 hover:text-blue-900 active:text-blue-900">
+      <span class="mr-2">Paypalでの寄付はこちら</span>
+      <Icon icon="mdi:open-in-new" width="22" height="22" />
+    </a>
+  </div>
+  <div class="container mx-auto mt-8 mb-24">
+    <form @submit.prevent="request">
       <div class="mb-4">
         <label for="name" class="block font-bold mb-2">お名前 <font color="red">*</font></label>
-        <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400" required>
+        <input type="text" id="name" name="name"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400"
+          required>
       </div>
       <div class="mb-4">
         <label for="email" class="block font-bold mb-2">メールアドレス <font color="red">*</font></label>
-        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400" required>
+        <input type="email" id="email" name="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400"
+          required>
       </div>
       <div class="mb-4">
         <fieldset role="radiogroup">
-        <label for="donate" class="block font-bold mb-2">寄付方法 <font color="red">*</font></label> 
+          <label for="donate" class="block font-bold mb-2">寄付方法 <font color="red">*</font></label>
           <SfSelect id="donate" name="donate" required>
             <option value="Amazon ギフト券">Amazon ギフト券</option>
             <option value="Apple ギフトカード">Apple ギフトカード</option>
@@ -56,21 +58,22 @@ import { ref } from 'vue';
       </div>
       <div class="mb-4">
         <label for="code" class="block font-bold mb-2">ギフトコード <font color="red">*</font></label>
-        <input type="code" id="code" name="code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400" required>
+        <input type="code" id="code" name="code"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-green-600 dark:placeholder-gray-400"
+          required>
       </div>
-      <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">送信</button>
+      <button :disabled="sent" type="submit"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">送信</button>
     </form>
-    <div
-    role="alert"
-    class="mt-8 flex items-start md:items-center shadow-md bg-positive-100 pr-2 pl-4 ring-1 ring-positive-200 typography-text-sm md:typography-text-base py-1 rounded-md"
-    v-if="sent"
-    >
+    <div role="alert"
+      class="mt-8 flex items-start md:items-center shadow-md bg-positive-100 pr-2 pl-4 ring-1 ring-positive-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+      v-if="sent">
       <SfIconCheckCircle class="my-2 mr-2 text-positive-700 shrink-0" />
       <p class="py-2 mr-2">フォームは正しく送信されました! 数秒後にリダイレクトします...</p>
     </div>
-    </div>
-    <basefooter />
-  </template>
+  </div>
+  <basefooter />
+</template>
 
 <script>
 import navbar from "@/components/layout/navbar.vue";
@@ -98,14 +101,14 @@ export default {
         console.error(e)
       })
     },
-    request: async function() {
+    request: async function () {
       var name = "entry.1454020375" + "=" + document.getElementById("name").value;
       var mail = "entry.2038478853" + "=" + document.getElementById("email").value;
       var method = "entry.750705808" + "=" + document.getElementById("donate").value;
       var code = "entry.1506693516" + "=" + document.getElementById("code").value;
-      
-      var url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSebVzQvkv_XYeIyHyNuzQeCsnNVHp6_2Y2OB1ykLTnxHZKSrg/formResponse?"+
-      name + "&" + mail + "&" + method + "&" + code;
+
+      var url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSebVzQvkv_XYeIyHyNuzQeCsnNVHp6_2Y2OB1ykLTnxHZKSrg/formResponse?" +
+        name + "&" + mail + "&" + method + "&" + code;
 
       if (name && mail && method && code) {
         axios.get(url);
@@ -125,14 +128,14 @@ export default {
 </script>
 
 <style scoped>
-h1{
-    font-size:32px;
+h1 {
+  font-size: 32px;
 }
-h2{
-    font-size:24px;
+
+h2 {
+  font-size: 24px;
 }
 
 .fadeout {
   opacity: 0;
-}
-</style>
+}</style>
