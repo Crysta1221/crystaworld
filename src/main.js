d32d00463028d087db67ea74637b8d8112e16a53
Crysta1221 <takuyakojim@outlook.jp>
2023-09-06 20:28:49 +0900

    Add Google Tag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from "./router"
 import '@fontsource/ibm-plex-sans-jp';
 import "@/styles/index.scss"
+import VueGtag from 'vue-gtag';
+import router from "./router"
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueGtag, { config: { id: "G-BET1HQG37V" }}).mount('#app')
